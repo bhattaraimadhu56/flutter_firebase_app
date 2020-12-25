0a8dc931b3f6f0a33894dc07db8607c4a01c50a0
Madhu <bhattaraimadhu56@gmail.comm>
2020-12-25 17:09:39 +1300

    simply define the routes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:madhuCabApp/screens/homePage.dart';
+import 'package:madhuCabApp/screens/loginPage.dart';
+import 'package:madhuCabApp/screens/registerPPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +11,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // to remove the debug sign
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-    );
+        // to remove the debug sign
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage(),
+        // In routes will pass key value pair String and WidgetBuilder
+        routes: <String, WidgetBuilder>{
+          //  To dublicate Shift+Alt+Arrow Down key
+          // "/login":(BuildContext context)=>LoginPage(),
+          "/login": (BuildContext context) => LoginPage(),
+          "/register": (BuildContext context) => RegisterPage(),
+        });
   }
 }
